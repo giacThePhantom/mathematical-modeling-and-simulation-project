4fd4c51afb20f99e82ecbebff52624af04594a0f
Alessandro <alg.polignano@gmail.com>
2023-01-20 18:36:39 +0100

    fixed supra threshold and synapse

--- src/synapse.m
+++ src/synapse.m
@@ -8,27 +8,50 @@ sim_time = 60; %s
 p_ampa = 0.875;
 p_gaba = 0.0625;
 p_nmda = 0.0625;
+opts = odeset('AbsTol', 1e-6, 'RelTol', 1e-6, 'InitialStep', 1e-8, 'MaxStep', 1e-2);
 
-init_cond = [-65; 0; 0; p_ampa/(1+p_ampa); p_gaba/(1+p_gaba); p_nmda/(1+p_nmda)];
+y_0 = [-73; 0; 0; 0; 0; 0];
 global s_array;
 global t_array;
 
-[t, v] = ode15s(@neuron, [0 100], init_cond);
+[T, Y] = ode15s(@(t, y) neuron(t, y), [0 100], y_0, opts);
 
 t_array = 0:100;
 
 for i=1:length(t_array),
-    s_array(end + 1) = compute_s(i);
+    s_array(end + 1) = s(i);
 end
 
-subplot(2, 1, 1)
-plot(t, v)
-legend("Voltage", "InterSpikeTime", "SK", "AMPA", "GABA", "NMDA")
-subplot(2, 1, 2)
+subplot(7, 1, 1)
+plot(T, Y(:, 1))
+legend("Voltage")
+
+subplot(7, 1, 2)
 plot(t_array, s_array)
+legend("Current")
+
+subplot(7, 1, 3)
+plot(T, Y(:, 2))
+legend("InterSpikeTime")
+
+subplot(7, 1, 4)
+plot(T, Y(:, 3))
+legend("SK")
+
+subplot(7, 1, 5)
+plot(T, Y(:, 4))
+legend("AMPA")
+
+subplot(7, 1, 6)
+plot(T, Y(:, 5))
+legend("GABA")
+
+subplot(7, 1, 7)
+plot(T, Y(:, 6))
+legend("NMDA")
 
 % Functions
-function dv = neuron(t, v)
+function dy = neuron(t, y)
 
     % Parameters
     E_L = -73; %mV
@@ -36,10 +59,9 @@ function dv = neuron(t, v)
     g_L = 0.025; %nS
     C = 0.375; %nF
     V_th = -53; %mV
-    alpha = 0;
-    gamma = 1;
-    s = compute_s(t);
-    beta_test = exp(-(v(2)^2)/(2*gamma^2));
+    alpha_m = 10000;
+    gamma_m = 1;
+    beta_m = @(T) exp(-(T^2)/(2*gamma_m^2));
 
     g_sk = 0.128; %nS
 
@@ -64,22 +86,45 @@ function dv = neuron(t, v)
     a = -53;
     b = 100;
 
-    dv = [
-       1/C*(g_L*(E_L - v(1)) + g_sk * v(3) *(E_sk - v(1)) + g_ampa * v(4) * (E_ampa - v(1)) + g_gaba * v(5) * (E_gaba - v(1)) + (g_nmda* v(6) * (E_nmda - v(1)))/(1+exp(-((v(1) - a)/(b))))) + alpha * (V_R-v(1)) * beta_test;
-       -alpha * v(2) * H(v(1), V_th);
-       ((1 - v(3)) * 4 * beta_test - v(3))/(tau_sk);
-       ((1 - v(4)) * (p_ampa + s) - v(4))/(tau_ampa);
-       ((1 - v(5)) * p_gaba - v(5))/(tau_gaba);
-       ((1 - v(6)) * p_nmda - v(6))/(tau_nmda);
-    ];
+    y = reshape(y, [], 6);
+    V = y(:, 1);
+    T = y(:, 2);
+    X_sk = y(:, 3);
+    X_ampa = y(:, 4);
+    X_gaba = y(:, 5);
+    X_nmda = y(:, 6);
+
+   dy = zeros(size(y));
+   dy(:, 1) = 1/C*(g_L*(E_L - V) + g_sk * X_sk *(E_sk - V) + g_ampa * X_ampa * (E_ampa - V) + g_gaba * X_gaba * (E_gaba - V) + (g_nmda* X_nmda * (E_nmda - V))/(1+exp(-((V - a)/(b))))) + alpha_m * (V_R-V) * beta_m(T);
+   dy(:, 2) = 1 - alpha_m * T * H(V, V_th);
+   dy(:, 3) = ((1 - X_sk) * 4 * beta_m(T) - X_sk)/(tau_sk);
+   dy(:, 4) = ((1 - X_ampa) * (p_ampa + s(t)) - X_ampa)/(tau_ampa);
+   dy(:, 5) = ((1 - X_gaba) * p_gaba - X_gaba)/(tau_gaba);
+   dy(:, 6) = ((1 - X_nmda) * p_nmda - X_nmda)/(tau_nmda);
+
+   dy = dy(:);
+
 end
 
-function s = compute_s(t)
-    if t > 20 && t < 40
-        s = 0000;
-    elseif t > 45 && t < 80
-        s = 0;
-    else, s = 0;
+function compute_s = s(t)
+    if t > 10 && t < 10.1
+        compute_s = 63770;
+    elseif t > 15 && t < 25
+        compute_s = 3;
+    elseif t > 30 && t < 32
+        compute_s = 3;
+    elseif t > 36 && t < 38
+        compute_s = 3; 
+    elseif t > 42 && t < 45
+        compute_s = 3;
+    elseif t > 50 && t < 52
+        compute_s = 1;
+    elseif t > 54 && t < 56
+        compute_s = 1; 
+    elseif t > 58 && t < 60
+        compute_s = 1;    
+    else
+        compute_s = 0;
     end
 end
 
