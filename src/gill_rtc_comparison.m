6565d74c2c3807fb42d25695ccd82660aaca5c72
Alessandro <alg.polignano@gmail.com>
2023-02-09 21:50:19 +0100

    Comparison of Gillespie simulations

--- src/gill_rtc_comparison.m
+++ src/gill_rtc_comparison.m
@@ -2,7 +2,7 @@
 [V2,M2,N2,t2,Mtot2,Ntot2] = ml_4_gill();
 
 csvwrite("rtc.csv", cat(1, V1, M1, N1).')
-csvwrite("gill.csv", cat(1, V2, M2, N2).')
+csvwrite("gill_tau_inside.csv", cat(1, V2, M2, N2).')
 
 figure
    plot(t1, V1, "g")
--- src/ml_4_gill.m
+++ src/ml_4_gill.m
@@ -1,51 +1,55 @@
-function [V,M,N,t,Mtot,Ntot]=ml_4_gill()
-    close all;
-    clear all;
+function [V,M,N,t,Mcalcium_tot,Npotassium_tot, tau_arr, eps_arr]=ml_4_gill(csv_name, tau_arr_in, eps_arr_in)
 
-    global Mcalcium_tot % total number of Calcium channels
-    global Mcalcium % number of Calcium channels in conducting state
-    global Npotassium_tot
-    global Npotassium
-    global tau1 T1
-    global tau2 T2
-    global tau3 T3
-    global tau4 T4
+    params = readtable(csv_name); 
 
-    Npotassium_tot = 100;
-    Ntot = Npotassium_tot;
-    
-    Mcalcium_tot = 100;
-    Mtot = Mcalcium_tot;
-    tmax=1e3;
+    global Mcalcium_tot Mcalcium
+    global Npotassium_tot Npotassium
+    global vK vL vCa
+    global gK gL gCa
+    global C
+    global tau
+    global I
 
     % Parameters
-    phi_m=0.4;
-    va = -1.2; vb=18;
-    vc = 2; vd = 30;
-    phi_n = 0.04;
+    tmax = params.tmax;
+
+    Npotassium_tot = params.nktot;
+    Mcalcium_tot = params.mcatot;
+
+    phi_m = params.phi_m; 
+    phi_n = params.phi_n;
+    va = params.va; 
+    vb = params.vb; 
+    vc = params.vc; 
+    vd = params.vd;
+
+    vK = params.vk; 
+    vL = params.vl; 
+    vCa = params.vca;
+    gK = params.gk;
+    gL = params.gl; 
+    gCa = params.gca;
+    C = params.c; 
+
+    V0 = params.v0; % start at an arbitrary middle voltage
+    M0 = params.m0; % start with calcium channels all closed
+    N0 = params.n0; % start with half potassium channels open
+
+    I = params.i;
+
+    t0=0;
 
     % Initial conditions
-    t0=0;
     t=t0; % global "external" time
-    tau1=-log(rand); % time of next event on reaction stream 1 ("internal time")
-    tau2=-log(rand); % time of next event on reaction stream 2 ("internal time")
-    tau3=-log(rand); % time of next event on reaction stream 3 ("internal time")
-    tau4=-log(rand); % time of next event on reaction stream 4 ("internal time")
-    T1=0; % integrated intensity function for reaction 1
-    T2=0; % integrated intensity function for reaction 2
-    T3=0; % integrated intensity function for reaction 3
-    T4=0; % integrated intensity function for reaction 4
-    V0=-50; % start at an arbitrary middle voltage
     V=V0; % initial voltage
-    M0=0; % start with calcium channels all closed
-    Mcalcium=M0; % initial state of calcium channel
     M=M0; % use M to record the time course
-    N0=ceil(Ntot/2); % start with half potassium channels open
-    Npotassium=N0; % initial state of potassium channel
     N=N0; % use N to record the time course
 
+    Mcalcium=M0; % initial state of calcium channel
+    Npotassium=N0; % initial state of potassium channel
+    
     % Morris Lecar
-    global Iapp; Iapp=@(t)100; % applied current
+    global Iapp; Iapp=@(t)I; % applied current
     global xi_m; xi_m=@(v)(v-va)/vb; % scaled argument for m-gate input voltage
     global minf; minf=@(v)0.5*(1+tanh(xi_m(v))); % m-gate activation function
     global tau_m; tau_m=@(v)1./(phi_m*cosh(xi_m(v)/2)); % m-gate time constant
@@ -57,9 +61,23 @@ function [V,M,N,t,Mtot,Ntot]=ml_4_gill()
     global alpha_n; alpha_n=@(v)(ninf(v)./tau_n(v));
     global beta_n; beta_n=@(v)((1-ninf(v))./tau_n(v));
 
-    options=odeset('Events',@nextevent);
+    options=odeset('Events', @nextevent);
 
+    index = 0;
+    tau_arr = tau_arr_in;
+    eps_arr = eps_arr_in;
     while t(end)<tmax
+        index = index + 1;
+        if length(tau_arr) < index
+            tau_arr = [tau_arr, -log(rand)]; 
+        end
+
+        if length(eps_arr) < index
+            eps_arr = [eps_arr, rand]; 
+        end
+
+        tau = tau_arr(index);
+        epsilon = eps_arr(index);
 
         U0=[V0;0;0;0;0;M0;N0];
         tspan=[t(end),tmax];
@@ -72,12 +90,12 @@ function [V,M,N,t,Mtot,Ntot]=ml_4_gill()
         M=[M,Mout'];
         N=[N,Nout'];
 
-        epsilon = rand;
 
-        lambda_0 = (alpha_n(V(end)) * (Ntot-N0)) + (beta_n(V(end)) * N0) + (alpha_m(V(end)) * (Mtot-M0)) + (beta_m(V(end)) * M0);
-        lambda_1 = alpha_n(V(end)) * (Ntot-N0);
+
+        lambda_0 = (alpha_n(V(end)) * (Npotassium_tot-N0)) + (beta_n(V(end)) * N0) + (alpha_m(V(end)) * (Mcalcium_tot-M0)) + (beta_m(V(end)) * M0);
+        lambda_1 = alpha_n(V(end)) * (Npotassium_tot-N0);
         lambda_2 = lambda_1 + (beta_n(V(end)) * (N0));
-        lambda_3 = lambda_2 + (alpha_m(V(end)) * (Mtot-M0));
+        lambda_3 = lambda_2 + (alpha_m(V(end)) * (Mcalcium_tot-M0));
 
         q_1 = lambda_1/lambda_0;
         q_2 = lambda_2/lambda_0;
@@ -85,9 +103,9 @@ function [V,M,N,t,Mtot,Ntot]=ml_4_gill()
 
         if (epsilon < q_1)
             N0=N0+1;
-        elseif (epsilon >= q_1 & epsilon < q_2)
+        elseif (epsilon >= q_1 && epsilon < q_2)
             N0=N0-1;   % increment channel state
-        elseif (epsilon >= q_2 & epsilon < q_3)
+        elseif (epsilon >= q_2 && epsilon < q_3)
             M0 = M0 + 1;
         else
             M0 = M0 - 1;
@@ -95,25 +113,22 @@ function [V,M,N,t,Mtot,Ntot]=ml_4_gill()
 
         Mcalcium=M0;
         Npotassium=N0;
-        if M0>Mcalcium_tot, error('M>Mtot'), end
+        if M0>Mcalcium_tot, error('M>Mcalcium_tot'), end
         if M0<0, error('M<0'), end
-        if N0>Npotassium_tot, error('N>Ntot'), end
+        if N0>Npotassium_tot, error('N>Npotassium_tot'), end
         if N0<0, error('N<0'), end
-        T1=T1+Uout(end,2);
-        T2=T2+Uout(end,3);
-        T3=T3+Uout(end,4);
-        T4=T4+Uout(end,5);
+
         V0=V(end);
 
     end % while t(end)<tmax
 
     %% Plot output
     figure
-    % subplot(6,1,1),plot(t,M), xlabel('Time'), ylabel('M'),
-    % subplot(6,1,2),plot(t,N), xlabel('Time'), ,ylabel('N')
-    % subplot(6,1,3),plot(t,V) ,xlabel('Time'), ylabel("V")
-    % subplot(6,1,4),plot(V,M,'.-'),xlabel('V'), ylabel('M')
-    % subplot(6,1,5),plot(V,N,'.-'),xlabel('V'), ylabel('M')
+    subplot(6,1,1),plot(t,M), xlabel('Time'), ylabel('M'),
+    subplot(6,1,2),plot(t,N), xlabel('Time'), ylabel('N')
+    subplot(6,1,3),plot(t,V) ,xlabel('Time'), ylabel("V")
+    subplot(6,1,4),plot(V,M,'.-'),xlabel('V'), ylabel('M')
+    subplot(6,1,5),plot(V,N,'.-'),xlabel('V'), ylabel('M')
 
 end
 
@@ -124,33 +139,26 @@ function dudt=dudtfunc(t,u)
     global Npotassium Npotassium_tot
     global alpha_m beta_m
     global alpha_n beta_n
-
-    %% Parameters
-    vK = -84; vL = -60; vCa = 120;
-    gK =8; gL =2; C=20; gCa = 4.4;
+    global vK vL vCa
+    global gK gL gCa
+    global C
 
     v=u(1); % extract the voltage from the input vect
-dudt=[...  % voltage
-    (Iapp(t)-gCa*(Mcalcium/Mcalcium_tot)*(v-vCa)-gL*(v-vL)...
-    -gK*(Npotassium/Npotassium_tot)*(v-vK))/C;
-    % Potassium chan. opening, internal time elapsed
-    alpha_n(v)*(Npotassium_tot-Npotassium);
-    % Potassium chan. closing, internal time elapsed
-    beta_n(v)*Npotassium;
-    % Calcium chan. opening, internal time elapsed
-    alpha_m(v)*(Mcalcium_tot-Mcalcium);
-    % Calcium chan. closing, internal time elapsed
-    beta_m(v)*Mcalcium;
-    0; % M is constant between events
-    0]; % N is constant between events
+dudt=[(Iapp(t)-gCa*(Mcalcium/Mcalcium_tot)*(v-vCa)-gL*(v-vL)-gK*(Npotassium/Npotassium_tot)*(v-vK))/C;
+       alpha_n(v)*(Npotassium_tot-Npotassium);     % Potassium chan. opening, internal time elapsed
+       beta_n(v)*Npotassium; % Potassium chan. closing, internal time elapsed
+       alpha_m(v)*(Mcalcium_tot-Mcalcium); % Calcium chan. opening, internal time elapsed
+       beta_m(v)*Mcalcium; % Calcium chan. closing, internal time elapsed
+        0; % M is constant between events
+        0]; % N is constant between events
 end
 
 % Define behavior at threshold crossing
 function [value,isterminal,direction] = nextevent(~,u)
-    global tau1 T1 % timing trigger for reaction 1 (opening)
-    global tau2 T2 % timing trigger for reaction 2 (closing)
-    global tau3 T3 % timing trigger for reaction 3 (opening)
-    value=[u(2)+u(3)+u(4)+u(5)-tau1];
+    
+    global tau
+
+    value=[u(2)+u(3)+u(4)+u(5)-tau];
     isterminal=[1]; % stop and restart integration at crossing
     direction=[1]; % increasing value of the quantity at the trigger
 end
