50d71ae0253eb63be079fb6252331199ef8f6eb8
Alessandro <alg.polignano@gmail.com>
2023-02-11 11:52:13 +0100

    hotfix

--- comparisons/scripts/generate_params.py
+++ comparisons/scripts/generate_params.py
@@ -17,7 +17,7 @@ for i in range(0, iterations):
     for y in range(0, iterations):
         new_file = open(f"../params_files/{int(k)}_{int(ca)}.csv", "w")
         cols = "tmax,nktot,mcatot,phi_m,phi_n,va,vb,vc,vd,vk,vl,vca,gk,gl,gca,c,v0,m0,n0,i\n"
-        params = [100.0,float(k),float(ca),0.4,0.04,-1.2,18.0,2.0,30.0,-84.0,-60.0,120.0,8.0,2.0,4.4,20.0,-50.0,0.0,int(float(k)/2),100.0]
+        params = [2000.0,float(k),float(ca),0.4,0.04,-1.2,18.0,2.0,30.0,-84.0,-60.0,120.0,8.0,2.0,4.4,20.0,-50.0,0.0,int(float(k)/2),100.0]
         new_file.writelines([cols, str(params)[1:len(str(params))-1].replace(" ", "")])
         new_file.close()
         if (y == 0):
