7493c1a13a06d784aa0f6004b9c4650f84838731
Alessandro <alg.polignano@gmail.com>
2023-02-14 02:37:18 +0100

    Comparisons results

--- src/gill_rtc_comparison.m
+++ src/gill_rtc_comparison.m
@@ -1,11 +1,16 @@
-[V1,M1,N1,t1,Mtot1,Ntot1] = ml_4_rtc();
-[V2,M2,N2,t2,Mtot2,Ntot2] = ml_4_gill();
+n_channels = ["params/1_1.csv", "params/10_10.csv", "params/50_50.csv", "params/100_100.csv"];
 
-csvwrite("rtc.csv", cat(1, V1, M1, N1).')
-csvwrite("gill_tau_inside.csv", cat(1, V2, M2, N2).')
+for i=1:length(n_channels)
 
-figure
-   plot(t1, V1, "g")
-   hold on;
-   plot(t2, V2, "b")
+   tau_mat = [ ; ; ; ]; 
+   [V1,M1,N1,t1,tau_mat] = ml_4_rtc(n_channels(i), tau_mat, 0);
+
+   tau_arr = [];
+   eps_arr = [];
+   [V2,M2,N2,t2,tau_arr, eps_arr] = ml_4_gill(n_channels(i), tau_arr, eps_arr);
+
+   csvwrite(join([n_channels(i) "rtc"], "_"), cat(1, V1, M1, N1).')
+   csvwrite(join([n_channels(i) "gill"], "_"), cat(1, V2, M2, N2).')
+
+end
 
